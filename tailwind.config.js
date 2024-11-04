1152bab2d2c2350e5f2f6f18bd421bdc4adece6d
IAMyanuar <ilhamyanuartaufiki27@gmail.com>
2024-11-04 21:59:03 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light"], // Atur tema default ke "light"
+  },
+};
